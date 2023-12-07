c7230574c1c80d22b50f149fe0c921f0e90660af
GibMeClay <raf.niedbalowski@gmail.com>
2023-12-07 14:49:21 +0100

    Added comments and completed TODO's

--- main.py
+++ main.py
@@ -3,31 +3,35 @@ from tkinter import ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
-
+# importowanie modułów potrzebnych do działania programów
 
 def update_plot(value):
+    # Główna funkcja aktualizująca wykresy reagująca na zmiane wartości na Sliderze
     dc_signal = float(value) * np.ones_like(time)
+    # Funkcja prądu stałego niezmienna w czasie
     line.set_ydata(dc_signal)
     ax.relim()
     ax.set_ylim([-10, 10])
     ax.set_xlim([0,5])
     canvas.draw()
+    # Wyświetlanie pierwszego wykresu po aktualizacji
     dual_slope = np.zeros_like(time)
     if float(value) > 0:
         indi=1
     else:
         indi=-1
+    # Sprawdzanie wartości napięcia stałego i nadanie wartości zmiennej modygikującej napięcie referencyjne.
+    Vref = 10 * indi # Napięcie referencyjne
+    splitValue = 2.5 # Stały czas pierwszej części ładowania kondensatora
+    StopV = splitValue * (float(value)) * indi # Już nie pamiętam co to jest ale jest potrzebne :shrug:
 
-    Vref = 10 * indi
-    splitValue = 2.5
-    StopV = splitValue * (float(value)) * indi
     z = (Vref * splitValue + StopV) / Vref
     x = (z - splitValue) * 400 * indi
     n = (x/400)+splitValue
     m=x/1000*Vref
+    # Obliczanie zmiennych potrzebnech w póżniejszych obliczeniach oraz w trakcie ewentualnego rozwoju programu
 
     for i in range(len(time)):
-
         if time[i] < splitValue:
             dual_slope[i] = ((-time[i]*float(value))/4)
         else:
@@ -35,18 +39,19 @@ def update_plot(value):
                  dual_slope[i] = ((-StopV*indi + Vref*(time[i]-splitValue))/4)
             else:
                  dual_slope[i] = 0
+                # Funkcje opisujące wygląd lini na wykresie po naładowaniu kondesatora
 
 
 
-#TODO co ustalam tutaj
-    line2.set_ydata(dual_slope)
-    plt.axvline(x=2.5, linestyle='dotted',linewidth= 0.5, color='red')
-    plt.axhline(y=0, color='r', linestyle='dotted' , linewidth= 0.5)
+    line2.set_ydata(dual_slope) # Przypisanie drugiemu wykresowi wartości
+    plt.axvline(x=2.5, linestyle='dotted',linewidth= 0.5, color='red') # Linia wskazująca na moment przełączenia z napięcia badanego na referencyjne
+    plt.axhline(y=0, color='r', linestyle='dotted' , linewidth= 0.5) # Linia wskazująca wartość y=0 dla łatwaiejszej widoczności pola pod/nad wykresem
     ax2.relim()
     ax2.set_ylim([-10, 10])
     ax2.set_xlim([0, 5])
-    canvas2.draw()
-#TODO Komentarz co tutaj się dzieje
+    canvas2.draw() # Ustalanie limity wykresu drugiego oraz go wywoływanie
+
+
     label = tk.Label(app, text="  x = " + "%.0f" % x + "  ",)
     label.after(1, label.destroy)
     label = tk.Label(app, text="  x = " "%.0f" % x + "  ",)
@@ -55,15 +60,18 @@ def update_plot(value):
     label.grid(row=3, column=3, columnspan=2, padx=10, pady=10)
     label1.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
     label2.grid(row=1, column=3, columnspan=2, padx=10, pady=10)
-# Create a time array from 0 to 1 with sufficient points
+    #ustawianie wszystkich zmiennych tekstów wyświetlających się w oknie Aplikacji
+
+#
+
+# Tworzę tablice od 0 do 5 posiadająca 1000 punktów
 time = np.linspace(0, 5, 1000)
 
-#i want to get rid of the X axis numbers
-# Create the main application window
+# Główne okno aplikacji
 app = tk.Tk()
-app.title("DC Signal Plotter")
+app.title("Praca Inżynierska")
 
-# Create a figure and axis for the plot
+# Pierwsza inicjalizacja oraz ustawianie wyglądu wykresów
 fig, ax = plt.subplots()
 fig2, ax2 = plt.subplots()
 canvas = FigureCanvasTkAgg(fig, master=app)
@@ -76,32 +84,32 @@ plt.tick_params(axis='x', which='both', bottom=True, top=True, labelbottom=False
 plt.xticks(np.arange(0, 5, step=0.05))
 plt.margins(x=0)
 
-# Add labels and title
-ax.set_ylabel('Voltage')
+# tytuły wykresów i osi
+ax.set_ylabel('Napięcie')
 ax.set_xlabel('Czas')
-ax.set_title('DC Signal')
+ax.set_title('Prąd stały')
 ax.set_xticks([])
-ax2.set_ylabel('Voltage')
+ax2.set_ylabel('Napięcie')
 ax2.set_xlabel('Czas')
 ax2.set_title('Dual Slope')
 
 
-# Add a slider to control the DC signal amplitude
+# Suwak odpowiedzialny za sterowanie napięcia
 amplitude_slider_label = ttk.Label(app, text="DC Signal Amplitude:")
-amplitude_slider = ttk.Scale(app, from_=-10, to=10, orient="horizontal", command=update_plot , length=300)
-amplitude_slider.set(0)  # Initial amplitude value
+amplitude_slider = ttk.Scale(app, from_=-10, to=10, orient="horizontal", command=update_plot , length=300) # Wygląd i zakres wartości na suwaku
+amplitude_slider.set(0)  # Wartość początkowa na suwaku
 
-# Arrange widgets using grid layout
+# Ustawianie lementów UI w siatce
 amplitude_slider_label.grid(row=0, column=0, padx=10, pady=10)
 amplitude_slider.grid(row=0, column=1, padx=10, pady=10)
 
-# Embed the Matplotlib plot in the Tkinter window
+# Umiejscowianie wykresów Matplotlib w oknie aplikacji
 canvas = FigureCanvasTkAgg(fig, master=app)
+# Ustawianie pozycji w oknie aplikacji
 canvas_widget = canvas.get_tk_widget()
 canvas_widget.grid(row=2, column=0, columnspan=2)
-
 canvas_widget2 = canvas2.get_tk_widget()
 canvas_widget2.grid(row=2, column=3, columnspan=2)
 
-# Run the Tkinter event loop
+# Zapętlanie
 app.mainloop()
